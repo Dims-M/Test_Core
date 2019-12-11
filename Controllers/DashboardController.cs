fb4958b4a390ed74e6e33c248d2b18bf417f72af
Дмитрий <rabolan@mail.ru>
2019-12-11 16:25:27 +0300

    добавлен элемент дашборда от эвотора

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Controllers
+{
+    /// <summary>
+    /// Основной контролер доски управления
+    /// </summary>
+    public class DashboardController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        //Основная панель
+            public IActionResult Summary()
+        {
+            return View();
+        }
+    }
+}

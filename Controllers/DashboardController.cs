5768fac984a767b7850d97087bbcf435e8e93592
Дмитрий <rabolan@mail.ru>
2019-12-12 12:55:27 +0300

    добавлен контекст связи бд. Вывод данных из бд. Вьюхи. миграции

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Test.Models;
 
 namespace Test.Controllers
 {
@@ -11,13 +12,18 @@ namespace Test.Controllers
     /// </summary>
     public class DashboardController : Controller
     {
+        MobileContext db; // Обьект для связи с БД
 
         public IActionResult Index()
         {
             return View();
         }
 
-      
+        //Конструктор контролера. При передачи контекста. Обращается к БД 
+      public DashboardController(MobileContext context)
+        {
+            db = context; 
+        }
 
         //Основная панель
         public IActionResult Summary()
@@ -25,7 +31,20 @@ namespace Test.Controllers
             return View();
         }
 
-        
+        /// <summary>
+        /// Получения списка всех товаров ихз БД
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult GetGoodsAlls()
+        {
+            return View(db.Phones.ToList()); // получаем список всех товаров
+        }
+
+       
+            public IActionResult Buy()
+        {
+            return View();
+        }
 
     }
 }

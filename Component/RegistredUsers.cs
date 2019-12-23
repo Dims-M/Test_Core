6dd078e9c4e718d50474effac280d192dbdf5ffb
Дмитрий <rabolan@mail.ru>
2019-12-23 14:17:18 +0300

    тестоаве компоненты

---
+++ Component/RegistredUsers.cs
@@ -0,0 +1,36 @@
+﻿using AsyncComponents.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncComponents.Components
+{
+    
+    /// <summary>
+    /// Компонет длявьюх
+    /// </summary>
+    
+    public class RegistredUsers : ViewComponent
+    {
+        private readonly UserService service;  //переменная для хранения промежудочных данных 
+
+        //конструктор
+        public RegistredUsers(UserService service)
+        {
+            this.service = service;
+        }
+        /// <summary>
+        /// Асинхронный запрос к сайту. скачиваение с сервера Json
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<User> users = await service.GetUsersAsync();
+            return View(users); // возвращаем лист
+        }
+
+
+    }
+}
---
+++ Models/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncComponents.Model
+{
+    /// <summary>
+    /// класс для скасивания и конвертации Json файла с тестового севака
+    /// </summary>
+    public class UserService
+    {
+        private string url = "https://jsonplaceholder.typicode.com/users";
+
+        /// <summary>
+        /// Метод получение спарсенного  Json файла
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<User>> GetUsersAsync()
+        {
+            HttpClient http = new HttpClient(); // для соединения и работы с сервером
+            HttpResponseMessage response = await http.GetAsync(url); // запрос в серверу
+           
+            string content = await response.Content.ReadAsStringAsync();  //преоброзование в строку.
+            return JsonConvert.DeserializeObject<List<User>>(content); //десиркализация строи в обьект <List<User>
+        }
+    }
+}

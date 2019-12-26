19b6bdbe5e84afe8a9b5208ba134533fbabfb5b9
Дмитрий <rabolan@mail.ru>
2019-12-26 16:22:18 +0300

    не рабочая форма проверки регисьррации

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,40 +38,13 @@ namespace Test.Controllers
         }
 
         //Контролер срабатывающий при регистраци. Откроется вьюха с формами регистрации
-        public IActionResult Registracion()
+        public IActionResult Registracion(RegistrationBindingModel model)
         {
             string[] sourse = { "Понедельник", "Втторник", "Среда", "Четверг", "Питница", "Суббота", "Воскрсенье", }; //Нужные данные. например из Базы данных
             SelectList selectListItems = new SelectList(sourse); // создаем специальны лист для выподающего списка
             ViewBag.SelectItems = selectListItems; //Создаем спец переменную ViewBag.SelectItems. В которой во вьюхе можно обращатсяпо имени
 
-            return View(); // 
-        }
-       
-        // Метод действия, который запустится, когда пользователь сделает submit формы (отправит POST запрос на сервер).
-        // При вызове данного метода, произойдет привязка модели, входящие данные в теле запроса, которые совпадают с названиями аргументов,
-        // будут записаны в эти аргументы.
-        [HttpPost]
-        public IActionResult Registration(RegistrationBindingModel model)
-        {
-            // Обработка полученных данных
 
-            Debug.WriteLine("Selected - " + selectListItems); //во вьюхе будет показана эта строка
-
-            //Debug.WriteLine("First Name = " + firstName);
-            //Debug.WriteLine("Last Name = " + lastName);
-            //Debug.WriteLine("Email = " + email);
-            //Debug.WriteLine("Phone Number = " + phoneNumber);
-
-            //Debug.WriteLine(model.Login);
-            //Debug.WriteLine(model.Email);
-            //Debug.WriteLine(model.Password);
-            //Debug.WriteLine(model.PasswordConfirm);
-            //Debug.WriteLine(model.TermsAccepted);
-
-
-            // ModelState.AddModelError - для HTML элементов, которые привязаны к свойству, указанному в первом параметре добавляется CSS класс input-validation-error
-            // ModelState.AddModelError - меняет состояние модели на невалидное
-            // ModelState.AddModelError - добавляет текст ошибки, связанный со свойством, указанном в первом параметре
             if (string.IsNullOrEmpty(model.Login))
             {
                 ModelState.AddModelError(nameof(model.Login), "Введите имя");
@@ -127,11 +100,98 @@ namespace Test.Controllers
             {
                 // если модель содержит значения противоречащие бизнес правилам - возвращаем то же самое представление с неправильными данными
                 // для того, чтобы пользователь мог их поправить
+                // return View(Registration(model)); // просто закрывается окно
                 return View(model);
             }
+            return View(); // 
+        }
+       
+        // Метод действия, который запустится, когда пользователь сделает submit формы (отправит POST запрос на сервер).
+        // При вызове данного метода, произойдет привязка модели, входящие данные в теле запроса, которые совпадают с названиями аргументов,
+        // будут записаны в эти аргументы.
+        [HttpPost]
+        public IActionResult Registration(RegistrationBindingModel model)
+        {
+            // Обработка полученных данных
+
+          //  Debug.WriteLine("Selected - " + selectListItems); //во вьюхе будет показана эта строка
+
+            //Debug.WriteLine("First Name = " + firstName);
+            //Debug.WriteLine("Last Name = " + lastName);
+            //Debug.WriteLine("Email = " + email);
+            //Debug.WriteLine("Phone Number = " + phoneNumber);
+
+            //Debug.WriteLine(model.Login);
+            //Debug.WriteLine(model.Email);
+            //Debug.WriteLine(model.Password);
+            //Debug.WriteLine(model.PasswordConfirm);
+            //Debug.WriteLine(model.TermsAccepted);
+
+
+            // ModelState.AddModelError - для HTML элементов, которые привязаны к свойству, указанному в первом параметре добавляется CSS класс input-validation-error
+            // ModelState.AddModelError - меняет состояние модели на невалидное
+            // ModelState.AddModelError - добавляет текст ошибки, связанный со свойством, указанном в первом параметре
+            //if (string.IsNullOrEmpty(model.Login))
+            //{
+            //    ModelState.AddModelError(nameof(model.Login), "Введите имя");
+            //}
+
+            //if (!string.IsNullOrEmpty(model.Login) && model.Login.Length > 50)
+            //{
+            //    ModelState.AddModelError(nameof(model.Login), "Значение имени не может привышать 50 символов");
+            //}
+
+            ////if (string.IsNullOrEmpty(model. LastName))
+            ////{
+            ////    ModelState.AddModelError(nameof(model.LastName), "Введите фамилию");
+            ////}
+
+            ////if (!string.IsNullOrEmpty(model.LastName) && model.LastName.Length > 50)
+            ////{
+            ////    ModelState.AddModelError(nameof(model.LastName), "Значение фамилии не может привышать 50 символов");
+            ////}
+
+            //if (string.IsNullOrEmpty(model.Email))
+            //{
+            //    ModelState.AddModelError(nameof(model.Email), "Введите Email");
+            //}
+
+            //if (!string.IsNullOrEmpty(model.Email) && !Regex.IsMatch(model.Email, @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"))
+            //{
+            //    ModelState.AddModelError(nameof(model.Email), "Не правильный формат Email");
+            //}
+
+            //if (string.IsNullOrEmpty(model.Password))
+            //{
+            //    ModelState.AddModelError(nameof(model.Password), "Введите пароль");
+            //}
+
+            //if (string.IsNullOrEmpty(model.PasswordConfirm))
+            //{
+            //    ModelState.AddModelError(nameof(model.PasswordConfirm), "Введите подтверждение пароля");
+            //}
+
+            //if (model.Password != model.PasswordConfirm)
+            //{
+            //    ModelState.AddModelError(nameof(model.Password), "Пароли не совпадают");
+            //}
+            //// Если модель прошла проверку и значения свойств соответствуют атрибутом установленным на модели,
+            //// то свойство ModelState.IsValid получает значение true
+            //if (ModelState.IsValid)
+            //{
+            //    Debug.WriteLine($"В базу записан пользователь: {model.Login}  (email: {model.Email}, tel: {model.Phone})");
+            //    return View("Success");
+            //}
+            //else
+            //{
+            //    // если модель содержит значения противоречащие бизнес правилам - возвращаем то же самое представление с неправильными данными
+            //    // для того, чтобы пользователь мог их поправить
+            //   // return View(Registration(model)); // просто закрывается окно
+            //    return View(model);
+            //}
 
           //  return View("Success", selectListItems); //во вьюхе серез модель будет отображатся выбранный день недели
-           // return View();
+            return View();
         }
 
         public IActionResult GetPci()
